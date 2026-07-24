ac05db5e9fa33ac9ac03f2b1b0d505315ab29515
Aditi kanojiya <alpesh.food@gmail.com>
2026-07-24 20:54:56 +0530

    Add dvc dependency for model pull on deploy

--- app/predictor.py
+++ app/predictor.py
@@ -1,16 +1,16 @@
 import joblib
 import pandas as pd
+from pathlib import Path
+
+MODEL_PATH = Path(__file__).resolve().parent.parent / "artifacts" / "model.pkl"
 
 
 class Predictor:
 
     def __init__(self):
-        self.model = joblib.load("artifacts/model.pkl")
+        self.model = joblib.load(MODEL_PATH)
 
     def predict(self, data):
-
         df = pd.DataFrame([data])
-
         prediction = self.model.predict(df)
-
         return float(prediction[0])
